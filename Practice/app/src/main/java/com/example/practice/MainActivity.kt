4b6dde79c4a8d98dc724ad751f09d0321dfe5fd2
Double.Pos <wks1945@naver.com>
2021-12-24 10:35:06 +0900

    Mobile start

---
+++ Practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tv: TextView = findViewById(R.id.tv_hello)
+        val btn: Button = findViewById(R.id.btn_kor)
+
+        btn.setOnClickListener{
+            tv.text = "안녕"
+        }
+
+
+//        val textView: TextView = findViewById(R.id.android_text) as TextView
+//        textView.setOnClickListener {
+//            textView.text = getString(R.string.name)
+//        }
+    }
+}
