f91c9ff659d5544fc36a131d616ec1657e6b8e6a
Double.Pos <wks1945@naver.com>
2021-12-26 23:37:56 +0900

    시간표시로직적용하기

--- Practice/app/src/main/java/com/example/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -4,18 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.*
+import kotlin.concurrent.timer
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var timerTask: Timer? = null
+
+        var isRunning = false
+        var sec : Int = 0
         val tv: TextView = findViewById(R.id.tv_hello)
         val btn: Button = findViewById(R.id.btn_kor)
 
-        btn.setOnClickListener{
-            tv.text = "안녕"
+        btn.setOnClickListener {
+            isRunning = !isRunning
+            if (isRunning == true) {
+                timerTask = kotlin.concurrent.timer(period = 1000) {
+                    sec++
+                    runOnUiThread {
+                        tv.text = sec.toString()
+                    }
+                }
+            } else {
+                timerTask?.cancel()
+            }
         }
+
+
 
 
 //        val textView: TextView = findViewById(R.id.android_text) as TextView
