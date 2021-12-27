3ed529b784602699fb155896f91746fbc458d44c
Double.Pos <wks1945@naver.com>
2021-12-27 10:28:00 +0900

    시간 random

--- Practice/app/src/main/java/com/example/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.lang.Math.abs
+import java.util.Random
 import java.util.*
-import kotlin.concurrent.timer
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,30 +17,32 @@ class MainActivity : AppCompatActivity() {
         var timerTask: Timer? = null
 
         var isRunning = false
-        var sec : Int = 0
-        val tv: TextView = findViewById(R.id.tv_hello)
-        val btn: Button = findViewById(R.id.btn_kor)
+        var sec: Int = 0
+        val tv: TextView = findViewById(R.id.tv_random)
+        val tv_t: TextView = findViewById(R.id.tv_timer)
+        val tv_p: TextView = findViewById(R.id.tv_point)
+        val btn: Button = findViewById(R.id.btn_main)
 
-        btn.setOnClickListener {
-            isRunning = !isRunning
-            if (isRunning == true) {
-                timerTask = kotlin.concurrent.timer(period = 1000) {
-                    sec++
-                    runOnUiThread {
-                        tv.text = sec.toString()
+        val random_box = Random()
+        val num = random_box.nextInt(1001)
+        tv.text = ((num.toFloat()) / 100).toString()
+
+
+            btn.setOnClickListener {
+                isRunning = !isRunning
+                if (isRunning == true) {
+                    timerTask = kotlin.concurrent.timer(period = 10) {
+                        sec++
+                        runOnUiThread {
+                            tv_t.text = (sec.toFloat() / 100).toString()
+                        }
                     }
+                } else {
+                    timerTask?.cancel()
+                    val point = abs(sec-num).toFloat()/100
+                    tv_p.text = point.toString()
                 }
-            } else {
-                timerTask?.cancel()
             }
         }
-
-
-
-
-//        val textView: TextView = findViewById(R.id.android_text) as TextView
-//        textView.setOnClickListener {
-//            textView.text = getString(R.string.name)
-//        }
     }
-}
+
